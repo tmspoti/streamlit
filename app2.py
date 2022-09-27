7c81b3e2b47499bbb8d5c9002ed9d7afc7aca13f
tmspoti <114470515+tmspoti@users.noreply.github.com>
2022-09-27 13:02:51 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -26,7 +26,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from dbo.datavis;")
 
 # Print results.
 for row in rows:
