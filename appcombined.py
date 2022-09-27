e74422be083ca3fc328eb431de02eb9aae016ed2
tmspoti <114470515+tmspoti@users.noreply.github.com>
2022-09-27 14:30:21 +0200

    Update appcombined.py

--- appcombined.py
+++ appcombined.py
@@ -26,8 +26,8 @@ def init_connection():
     )
 conn = init_connection()
 
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+ Perform query.
+ Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
@@ -41,53 +41,55 @@ df.columns =['KorosionsrateSilber', 'RestschichtdickeSilber', 'KorosionsrateKupf
 
 st.write(df.head(10))
 
-start_time = st.slider(
-     "Zeitraum auswählen",
-     value=(datetime(2020, 7, 20, 0, 0), datetime(2020, 9, 5, 10, 00)),
-     format="MM/DD/YY")
-# st.write("Start time:", start_time)
-start_date = start_time[0]
-end_date = start_time[0]
+df.plot(df.t, df.Temperatur)
 
-def plot():
-
-    # alle verschiedenen Dateinamen
-    clist = df["Name"].unique().tolist()
-
-
-    # Dropdownmenü zur Auswahl
-    countries = st.sidebar.multiselect("Sensor auswählen", clist)
-    st.subheader("Auswahl: {}".format(", ".join(countries)))
-
-    dfs = {country: df[df["Name"] == country] for country in countries}
-
-    # chart 1
-    figvibr = go.Figure(layout=go.Layout(title='Vibration zu Zeit', yaxis = dict(
-      title = 'Vibration',
-      showgrid = True,
-      zeroline = True,
-      showline = True,
-      showticklabels = True,
-      gridwidth = 1 )))
-
-    for country, df in dfs.items():
-        figvibr = figvibr.add_trace(go.Scatter(x=df["Datetime"], y=df["Vibration RMS Velocity"], name=country))
-
-    # chart 2
-    figtemp = go.Figure(layout=go.Layout(title='Temperatur nach Zeit', yaxis = dict(
-      title = 'Temperatur',
-      showgrid = True,
-      zeroline = True,
-      showline = True,
-      showticklabels = True,
-      gridwidth = 1 )))
-    for country, df in dfs.items():
-        figtemp = figtemp.add_trace(go.Scatter(x=df["Datetime"], y=df["Temperature" ], name=country))
-
-    col1, col2 = st.columns(2)
-
-    st.plotly_chart(figvibr)    # chart 1
-    st.plotly_chart(figtemp)    # chart 2
-
-
-plot()
+#start_time = st.slider(
+#     "Zeitraum auswählen",
+#     value=(datetime(2020, 7, 20, 0, 0), datetime(2020, 9, 5, 10, 00)),
+#     format="MM/DD/YY")
+## st.write("Start time:", start_time)
+#start_date = start_time[0]
+#end_date = start_time[0]
+#
+#def plot():
+#
+#    # alle verschiedenen Dateinamen
+#    clist = df["Name"].unique().tolist()
+#
+#
+#    # Dropdownmenü zur Auswahl
+#    countries = st.sidebar.multiselect("Sensor auswählen", clist)
+#    st.subheader("Auswahl: {}".format(", ".join(countries)))
+#
+#    dfs = {country: df[df["Name"] == country] for country in countries}
+#
+#    # chart 1
+#    figvibr = go.Figure(layout=go.Layout(title='Vibration zu Zeit', yaxis = dict(
+#      title = 'Vibration',
+#      showgrid = True,
+#      zeroline = True,
+#      showline = True,
+#      showticklabels = True,
+#      gridwidth = 1 )))
+#
+#    for country, df in dfs.items():
+#        figvibr = figvibr.add_trace(go.Scatter(x=df["Datetime"], y=df["Vibration RMS Velocity"], name=country))
+#
+#    # chart 2
+#    figtemp = go.Figure(layout=go.Layout(title='Temperatur nach Zeit', yaxis = dict(
+#      title = 'Temperatur',
+#      showgrid = True,
+#      zeroline = True,
+#      showline = True,
+#      showticklabels = True,
+#      gridwidth = 1 )))
+#    for country, df in dfs.items():
+#        figtemp = figtemp.add_trace(go.Scatter(x=df["Datetime"], y=df["Temperature" ], name=country))
+#
+#    col1, col2 = st.columns(2)
+#
+#    st.plotly_chart(figvibr)    # chart 1
+#    st.plotly_chart(figtemp)    # chart 2
+#
+#
+#plot()
